999210a237a933d148dc2b58cef39bec8d0c2cd0
Andrew Cunliffe <andrew@sysen.co.uk>
2019-07-10 21:22:47 +0100

    defect(schedule): fix months field for dates (#59)

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -13,31 +13,31 @@ export class ScheduleComponent {
             type: 'stream',
             title: 'Code Mortals Website (Schedule)',
             link: 'https://www.youtube.com/watch?v=WVzwX3_jJPc',
-            date: new Date(2019, 7, 10, 17, 0, 0),
+            date: new Date(2019, 6, 10, 17, 0, 0),
         },
         {
             type: 'conference',
             title: 'Serverless Days',
             link: 'https://london.serverlessdays.io',
-            date: new Date(2019, 7, 11, 10, 0, 0),
+            date: new Date(2019, 6, 11, 10, 0, 0),
         },
         {
             type: 'stream',
             title: '4 Developers joining us to discuss entering their careers',
             link: 'https://www.youtube.com/channel/UCtjddf97i066cJVcloZ4B7g',
-            date: new Date(2019, 7, 15, 18, 30, 0),
+            date: new Date(2019, 6, 15, 18, 30, 0),
         },
         {
             type: 'meetup',
             title: 'Linux from Scratch - Linux 103 - the CLI and beyond',
             link: 'https://www.meetup.com/Linuxing-In-London/events/262372452/',
-            date: new Date(2019, 7, 17, 18, 0, 0),
+            date: new Date(2019, 6, 17, 18, 0, 0),
         },
         {
             type: 'stream',
             title: 'Developers vs Recruiters - Part 2',
             link: 'https://www.youtube.com/channel/UCtjddf97i066cJVcloZ4B7g',
-            date: new Date(2019, 7, 24, 18, 30, 0),
+            date: new Date(2019, 6, 24, 18, 30, 0),
         }
     ];
 
