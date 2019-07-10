07aa987945d214c1204998fc76432470a4df3f93
Andrew Cunliffe <andrew@sysen.co.uk>
2019-07-10 20:52:19 +0100

    feat(schedule): added initial schedule and navigation (#59)

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { LandingComponent } from './landing/landing.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
 const routes: Routes = [
+    {
+        path: 'schedule',
+        component: ScheduleComponent
+    },
     {
         path: '**',
         component: LandingComponent,
