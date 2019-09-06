9740e126651a00622a3d695e91c8074b434cbecb
Andrew <andrew@sysen.co.uk>
2019-09-06 21:45:59 +0100

    feat(profile): ability for users to manage their profile (#87)
    * feat(profile): ability for users to manage their profile (#87)
    
    * chore(lint): fix some types that were missing (#87)

--- src/services/models/user-profile.ts
+++ src/services/models/user-profile.ts
@@ -1,6 +1,17 @@
+import * as firebase from 'firebase';
+
 export interface UserProfile {
+    uid: string;
     alias: string;
     email: string;
     avatar: string;
-    since: Date;
+    social?: {
+        github?: string;
+        twitter?: string;
+        youtube?: string;
+        facebook?: string;
+        linkedin?: string;
+        instagram?: string;
+    };
+    since: firebase.firestore.Timestamp;
 }
---
+++ src/services/profile.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+import { from, Observable } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+
+import { UserProfile } from '@cm/services/models';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ProfileService {
+
+    constructor(
+        private firestore: AngularFirestore,
+    ) { }
+
+    public retrieve(userId: string): Observable<UserProfile> {
+        return this.firestore.collection<UserProfile>('users').doc<UserProfile>(userId).valueChanges();
+    }
+
+    public update(profile: UserProfile): Observable<UserProfile> {
+        const query = this.firestore.collection<UserProfile>('users').doc<UserProfile>(profile.uid).set(profile, { merge: true });
+        return from(query).pipe(mergeMap(() => this.retrieve(profile.uid)));
+    }
+
+}
